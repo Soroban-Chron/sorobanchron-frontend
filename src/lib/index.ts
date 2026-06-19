68213fe2c9122c60903f40b5e59d34434e937da3
Developer <dev@soroban-chron.local>
2026-06-19 13:59:34 +0000

    refactor: add lib barrel export

---
+++ src/lib/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Barrel exports for all lib utilities
+ * Usage: import { formatDate, isValidAddress, createLogger } from '@/lib'
+ */
+
+export * from "./async";
+export * from "./constants";
+export * from "./errors";
+export * from "./formatting";
+export * from "./logger";
+export * from "./network";
+export * from "./storage";
+export * from "./theme";
+export * from "./validation";
